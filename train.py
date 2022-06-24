af01b2e7951ab227712d9c839b85076178bcd5ca
Elham Madjidi <42636286+elhamadjidi@users.noreply.github.com>
2022-06-24 17:17:05 -0500

    updated train.py

--- train.py
+++ train.py
@@ -31,11 +31,11 @@ export_path = os.path.join(MODEL_DIR)
 print('export_path = {}\n'.format(export_path))
 
 
-# filename = f'{export_path}/paragh_model.sav'
+filename = f'{export_path}/model_pkl'
 # pickle.dump(model, open(filename, 'wb'))
 import pickle
 # create an iterator object with write permission - model.pkl
-with open(f'{export_path}/model_pkl', 'wb') as files:
+with open(filename, 'wb') as files:
     pickle.dump(model, files)
 
 print('\nModel saved to ' + MODEL_DIR)
